dba5fe4490ac32850290524be532bcc147b87a3f
Bahyun Lee <bahyun.lee@gmail.com>
2016-02-01 00:05:56 +0900

    secure
    upload file str_replace '.' -> ''

--- proc_file.php
+++ proc_file.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 	die("Unusual request method");
 }
 $basedir_upload = "./files/";
-$tmp_name_file = $_FILES["file"]["tmp_name"];
+$tmp_name_file = str_replace(".", "", $_FILES["file"]["tmp_name"]);
 $dir_upload = $basedir_upload.basename($tmp_name_file);
 $result = move_uploaded_file($tmp_name_file, $dir_upload);
 if ($result == true) {
