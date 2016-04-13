bed0221933dde2ef5f85af56c133d4bcca9619be
Bahyun Lee <bahyun.lee@gmail.com>
2016-04-14 00:21:11 +0900

    remove comment
    - remove comment.

--- file_upload.php
+++ file_upload.php
@@ -17,10 +17,6 @@
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
 	exit;
 }
-// $basedir_upload = "./files/";
-// $tmp_name_file = str_replace(".", "", $_FILES["file"]["tmp_name"]);
-// $dir_upload = $basedir_upload.basename($tmp_name_file);
-// $result = move_uploaded_file($tmp_name_file, $dir_upload);
 $base_path = "./files/"
 $file_name = str_replace(".", "", $_FILES["file"]["tmp_name"]);
 $result = move_uploaded_file($file_name, $base_path.$file_name);
