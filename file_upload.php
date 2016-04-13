235e2ca88c25b3bf09569d6587c6742ffa5b9647
Bahyun Lee <bahyun.lee@gmail.com>
2016-04-13 23:45:01 +0900

    combine file_upload.php and proc_file.php
    + proc_file.php code.

--- file_upload.php
+++ file_upload.php
@@ -1,6 +1,7 @@
 <!DOCTYPE xhtml>
 <html>
 	<head>
+		<meta charset="utf-8" content="text/html" http-equiv="Content-Type" />
 		<title> File upload </title>
 	</head>
 	<body>
@@ -12,3 +13,20 @@
 	</form>
 	</body>
 </html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+	exit;
+}
+// $basedir_upload = "./files/";
+// $tmp_name_file = str_replace(".", "", $_FILES["file"]["tmp_name"]);
+// $dir_upload = $basedir_upload.basename($tmp_name_file);
+// $result = move_uploaded_file($tmp_name_file, $dir_upload);
+$base_path = "./files/"
+$file_name = str_replace(".", "", $_FILES["file"]["tmp_name"]);
+$result = move_uploaded_file($file_name, $base_path.$file_name);
+if ($result == true) {
+	echo("success");
+} else {
+	echo("fail");
+}
+?>
