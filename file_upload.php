76f14f3c3fb1d6d7af445d08754091fc1ddff7ac
Bahyun Lee <bahyun.lee@gmail.com>
2016-04-14 00:20:47 +0900

    form action edit
    / action : proc_file.php -> file_upload.php.

--- file_upload.php
+++ file_upload.php
@@ -5,7 +5,7 @@
 		<title> File upload </title>
 	</head>
 	<body>
-	<form action="proc_file.php" enctype="multipart/form-data" method="post">
+	<form action="file_upload.php" enctype="multipart/form-data" method="post">
 		<input name="MAX_FILE_SIZE" type="hidden" value="7578" />
 		File upload :
 		<input name="upload" type="file" /><br />
