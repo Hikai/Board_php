a4eea6964a84ccea67270bc30acd11297ef34b72
Bahyun Lee <bahyun.lee@gmail.com>
2016-05-08 02:53:01 +0900

    mysql -> mysqli function.
    / substitute mysql function to mysqli function in php.

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$db_passwd = "password";
-	$conn = @mysql_connect("localhost", "root", $db_passwd) or die("Connect error");
-	$db_select = @mysql_select_db("db_board", $conn) or die ("DB select error");
+$db_passwd = "password";
+$conn = @mysqli_connect("localhost", "root", $_dbpasswd) or die("Connect error");
+$db_select = @mysqli_select_db($conn, "db_admin") or die("DB select error");
 ?>
